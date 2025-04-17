7162257ff124d52337d8d78b2248d5560fbdf69a
Nairolf13 <fb_du_13@live.fr>
2025-04-17 17:14:26 +0200

    add port in .env

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: import.meta.env.PORT,
     strictPort: true // échoue si le port est déjà pris
   }
 });
