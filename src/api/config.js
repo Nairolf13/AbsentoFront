38cd7f75a10d3ecaeb0bae5ffc439b3dca884a3d
Nairolf13 <fb_du_13@live.fr>
2025-04-25 00:31:04 +0200

    update process.env

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 axios.defaults.baseURL = API_URL;
 axios.defaults.withCredentials = true;
-
-export default API_URL;
